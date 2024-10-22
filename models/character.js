cb145ae2ba0b5e2d211a66935b6f29aa87c9be46
Jeyabal <jeyabal1993@gmail.com>
2024-10-21 23:59:17 -0400

    Updating character object

--- models/character.js
+++ models/character.js
@@ -6,21 +6,58 @@ const characterSchema = mongoose.Schema({
     charId: Number,
     gender: String,
     class: String,
-    race: String,
-    maxHitPoints: Number,
-    currentHitPoints: Number,
-    temporaryHitPoints: Number,
+    race: {
+        baseRace: String,
+        raceId: Number,
+        raceDescription: String,
+        isSubRace: Boolean,
+        subRace: String,
+        subRaceId: Number,
+        subRaceDescription: String,
+        racialTraits: [{}]
+    },    
     alignmentId: Number,
     lifeStyleId: Number,
+    faith: String,
+    appearance: {
+        height: String,
+        weight: String,
+        eyes: String,
+        skin: String,
+        hair: String,
+        facialHair: String,       
+        birthmarks: String
+    },
     backgroundInfo: {},
     inventory: {},
     currencies: {
         sp: Number,
         gp: Number
     },
+    graphics: {
+        displayAvatarUrl: String,
+        frame: String,
+        backdrop:{
+            backdropUrl: String
+        },
+        theme: String
+    },
     feats: String,
     spellSlots: Number,
-    abilityScores: {}
+    abilityScores: {},
+    maxHitPoints: Number,
+    currentHitPoints: Number,
+    temporaryHitPoints: Number,
+    bonusHitPoints: Number,
+    currentXp: Number,
+    speeds:{
+        walkingSpeed: Number,
+        runningSpeed: Number,
+        walkingSpeed: Number,
+        walkingSpeed: Number,
+    }
+    
+
 });
 
 module.exports = mongoose.model('Characters', characterSchema);
