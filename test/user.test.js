83b1be154104abc73e802fd44297a8ecd8cb5693
Jeyabal <jeyabal1993@gmail.com>
2024-10-27 22:45:54 -0400

    Adding a test case for delete user, pending

--- test/user.test.js
+++ test/user.test.js
@@ -1,12 +1,20 @@
-import { describe, test, it, expect } from "vitest";
+import { describe, test, it, expect, vi } from "vitest";
 
 const user = require('../controllers/userController');
+
+describe('TestDeleteUser', () => {
+    it('should delete the user data', async () => {
+        const result = await user.deleteUser({body: {userId: 1000}});
+        console.log('User deleted. Result received :', result);
+        //expect(result.status).toBe(204);
+    });
+}, 5000)
+
 
 describe('TestGetAllUsers', () => {
     it('should return all the user data', async () => {
         const users = await user.getAllUsers();
         console.log('Users found while unit testing getAllUsers: ', users);
-        expect(users).toBeNull;
+        expect(users).not.toBeNull()
     });
-}, 10000)
-
+}, 5000)
